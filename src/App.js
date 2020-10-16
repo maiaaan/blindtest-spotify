4cf1d5e2ae8cbeb4e526cb4c3e0308462941a59b
Mai-An <maianguyen8@gmail.com>
2020-10-16 08:42:07 +0200

    version sur mon ordi et affichage titre chanson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 /*global swal*/
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import loading from './loading.svg';
 import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+const apiToken = 'BQBZWJjRDoCekdWZXSk4Z5W9Q_PxiBOxnk6fBrIW4Bc0zdT2uqn7-zbJdAUKyXM8AgAVvxBt2ucTEe8hp3-jVW8d2Qt10pRQ4J58dyN3Gh7cBliWyeUQnQT2lX5Z75k6cw6hG_SGGue_sbfQe3BFm1dfgQs';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -30,19 +30,70 @@ function getRandomNumber(x) {
 }
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">no no no</h1>
-      </header>
-      <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+  const [text, setText] = useState('');
+  useEffect ( () => {
+    setText ("Bienvenue");
+  }, [] )
+
+  const [tracks, setTracks] = useState('');
+
+  const [songsLoaded, setSongsLoaded] = useState(false);
+
+  useEffect ( () => {
+    fetch('https://api.spotify.com/v1/me/tracks', {
+      method: 'GET',
+      headers: {
+       Authorization: 'Bearer ' + apiToken,
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log("Réponse reçue ! Voilà ce que j'ai reçu : ", data);
+        console.log(data.items.length);
+        setTracks (data.items);
+        setSongsLoaded (true);
+      })
+  }, [] )
+
+  const AlbumCover = (props) =>  {
+    const src = "https://example.com/image.png"; // A changer ;)
+    return (
+        <img src={src} style={{ width: 400, height: 400 }} />
+    );
+  }
+
+  if (songsLoaded) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">no no no</h1>
+        </header>
+        <div className="App-images">
+          <p>{text}</p>
+          <p>{tracks[0].track.name} - {tracks[0].track.artists[0].name}</p>
+          <p>{}</p>
+          <AlbumCover />
+        </div>
+        <div className="App-buttons">
+        </div>
       </div>
-      <div className="App-buttons">
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={loading} className="App-logo" alt="logo"/>
+          <h1 className="App-title">no no no</h1>
+        </header>
+        <div className="App-images">
+          <p>{text}</p>
+        </div>
+        <div className="App-buttons">
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
